c6964eb6e2eebb82b6734bc55f03980b8a76127c
kobol20 <fabian.herrera.panta@gmail.com>
2021-03-22 12:30:49 -0300

    modificacion estructura

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require('path'); // NodeJS Package for file paths
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.set("port", process.env.PORT || 3000);
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(__dirname + '/public')); // Provide static directory for frontend
 
 // Routes
 app.use("/api/users", require("./routes/user.routes"));
